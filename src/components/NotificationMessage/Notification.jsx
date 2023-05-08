c73070b7ddf610595a02eab3e275cb959d9cc6cf
Mikhailo Salko <slipknotmisha@gmail.com>
2023-05-08 21:02:54 +0300

    change className

--- src/components/NotificationMessage/Notification.jsx
+++ src/components/NotificationMessage/Notification.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import styles from './Notification.module.css';
 
 const Notification = ({ message }) => {
-  return <p className={styles.text}>{message}</p>;
+  return <p className={styles.messageText}>{message}</p>;
 };
 
 export default Notification;
